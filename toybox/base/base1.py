740475ae1f2c125e3da4a0fcc013c83cf53064ff
Danilo Naiff <danilonaiff1992@gmail.com>
2021-10-25 17:12:28 -0300

    New commit

--- toybox/base/base1.py
+++ toybox/base/base1.py
@@ -10,12 +10,12 @@ from pyequion import builder
 from pyequion import converters
 
 
-eqsys = EquilibriumSystem(['C','Ca', 'Na', 'Cl'], from_elements=True, activity_model="PITZER")
+eqsys = EquilibriumSystem(['C','Ca', 'Na', 'Cl'], from_elements=True, activity_model="DEBYE")
 elements_balance = {'Ca':0.028, 'C':0.065, 'Na':0.075, 'Cl':0.056}
 #species_balance = {'Ca++':0.028, 'Cl-':0.056, 'Na+':0.075, 'HCO3-':0.065}
 TK = 298.15
 solution1, res = eqsys.solve_equilibrium_elements_balance(TK, elements_balance, tol=1e-12)
 print(solution1)
 #print(solution.saturation_indexes)
-#solution2, res = eqsys.solve_equilibrium_elements_balance_phases(TK, elements_balance, tol=1e-12)
-solution3, res = eqsys.solve_equilibrium_elements_balance_phases(TK, elements_balance, tol=1e-12, solid_phases=[])
+solution2, res = eqsys.solve_equilibrium_elements_balance_phases(TK, elements_balance, tol=1e-12, has_gas_phases=False)
+#solution3, res = eqsys.solve_equilibrium_elements_balance_phases(TK, elements_balance, tol=1e-12, solid_phases=[])

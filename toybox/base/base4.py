ec9e601858f74d4f550142f47c01ca777207db9e
Danilo Naiff <danilonaiff1992@poli.ufrj.br>
2022-03-23 20:11:53 -0300

    Remove ignored files

--- toybox/base/base4.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import pyequion
-
-default_files = pyequion.builder.DEFAULT_DB_FILES
-possible_reactions = pyequion.builder.get_all_possible_reactions(default_files)
-possible_species = {k for reac in possible_reactions for k in reac.keys() if pyequion.builder._check_validity_specie_tag_in_reaction_dict(k)}
--- toybox/gui/run.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-import pyequion
-
-pyequion.rungui()
